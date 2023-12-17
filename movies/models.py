05e99df3196aa241ca028dd4dc19a624a75c2467
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-12-17 12:51:40 +0330

    installing apps in setting.py/creating movie mode in models.py/adding it to admin.py

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MovieData(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=200)
+    genres = models.CharField(max_length=300)
+    duration = models.FloatField()
+    rating = models.FloatField()
+    release_date = models.DateField()
